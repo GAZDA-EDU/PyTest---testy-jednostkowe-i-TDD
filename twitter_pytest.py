0d8c5a5c14dafae102dcf02f90373fb0c66c4f50
Marcin Gazdowski <gazda.github@gmail.com>
2021-10-09 10:18:19 +0200

    Fixtures - reużywalne fragmenty kodu.

--- twitter_pytest.py
+++ twitter_pytest.py
@@ -2,21 +2,34 @@ import pytest
 
 from twitter import Twitter
 
-def test_twitter_initialization():
+
+@pytest.fixture(scope='function')
+def twitter(request):
     twitter = Twitter()
+
+    def fin():
+        twitter.delete()
+
+    request.addfinalizer(fin)
+    yield twitter
+    twitter.delete()
+
+
+def test_twitter_initialization(twitter):
     assert twitter
 
-def test_tweet_single_message():
-    twitter = Twitter()
+
+def test_tweet_single_message(twitter):
     twitter.tweet('Test message')
     assert twitter.tweets == ['Test message']
 
-def test_tweet_log_message():
-    twitter = Twitter()
+
+def test_tweet_log_message(twitter):
     assert twitter
     with pytest.raises(Exception):
-        twitter.tweet('test'*41)
+        twitter.tweet('test' * 41)
         assert twitter.tweets == []
+
 
 @pytest.mark.parametrize("message, expected", (
         ("Test #first message", ['first']),
@@ -25,6 +38,5 @@ def test_tweet_log_message():
         ("Test message #first", ['first']),
         ("Test message #first #second", ['first', 'second'])
 ))
-def test_tweet_with_hashtag(message, expected):
-    twitter = Twitter()
+def test_tweet_with_hashtag(twitter, message, expected):
     assert twitter.find_hashtags(message) == expected
